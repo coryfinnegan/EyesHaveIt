8468bf8b7fe396229d57f3c176b6256545bc7dae
Ryan Finnegan <rfinnegan@blizzard.com>
2019-02-11 11:22:33 -0600

    Fixing nez effects

--- Scenes/MainMenu.cs
+++ Scenes/MainMenu.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Nez;
+﻿using Nez;
 using Microsoft.Xna.Framework;
 using Nez.BitmapFonts;
 using Nez.UI;
